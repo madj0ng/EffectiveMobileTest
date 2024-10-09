7eca3a7ae2697cfd7128790d242e8fd0cea16b00
madj0ng <madj0ng@yandex.ru>
2024-10-09 16:33:32 +0300

    Layout of the search page and list items

--- app/src/main/java/ru/madj0ng/effectivemobiletest/ui/RootActivity.kt
+++ app/src/main/java/ru/madj0ng/effectivemobiletest/ui/RootActivity.kt
@@ -2,7 +2,10 @@ package ru.madj0ng.effectivemobiletest.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.Group
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.madj0ng.effectivemobiletest.R
 
 class RootActivity : AppCompatActivity() {
@@ -12,5 +15,8 @@ class RootActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
